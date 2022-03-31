dd145633783afd0ab164eba5a9fcacbe193deec9
Kevin <smartkevin2003@gmail.com>
2022-03-31 22:31:45 +0530

    Finish UI

--- scripts.js
+++ scripts.js
@@ -1,51 +1,88 @@
-let weapons = ['rock', 'paper', 'scissors'];
+
+// Getting choice by computer
 function computerPlay(){
+    const weapons = ['rock', 'paper', 'scissors'];
     return weapons[Math.floor(Math.random()*3)];
 }
 
-function play(playerSelection, computerSelection){
-    playerSelection = playerSelection.toLowerCase();
-    while(!weapons.includes(playerSelection)){
-        console.log('Enter a valid choice!');
-        playerSelection = prompt("Enter Rock, Paper or Scissors!").toLowerCase();
-    }
-    if(playerSelection==='rock' && computerSelection==='scissors')
-    return "You Win! Rock beats Scissors!";
-    if(playerSelection==='rock' && computerSelection==='paper')
-    return "You Lose! Paper beats Rock!";
-    if(playerSelection==='paper' && computerSelection==='scissors')
-    return "You Lose! Scissors beats Paper!";
-    if(playerSelection==='paper' && computerSelection==='rock')
-    return "You Win! Paper beats Rock!";
-    if(playerSelection==='scissors' && computerSelection==='paper')
-    return "You Win! Scissors beats Paper!";
-    if(playerSelection==='scissors' && computerSelection==='rock')
-    return "You Lose! Rock beats Scissors!";
-    if(playerSelection===computerSelection)
-    return "Tie!";
+// Called when one of the players get score:5
+function gameEnd(resultText){
+    playerChoices.forEach(choice => choice.classList.remove('playerChoice:hover'));
+    finalResultText.textContent = resultText;
+    finalResult.classList.add('showResult');
+    playAgainBtn.addEventListener('click', restart);
+
 }
 
-function gamePlay(){
-    let playerScore = 0, computerScore = 0, result;
-    for(let i=0; i<5; i++){
-        const playerSelection = prompt("Enter Rock, Paper or Scissors!");
-        const computerSelection = computerPlay();
-        result = play(playerSelection, computerSelection);
-        if(result.includes('You Win!')) playerScore += 1;
-        else if(result.includes('You Lose!')) computerScore += 1;
-        console.log(`Round ${i+1}`);
-        console.log(`Player: ${playerSelection} || Computer: ${computerSelection}`);
-        console.log(result);
-        console.log(`PlayerScore: ${playerScore} || ComputerScore: ${computerScore}`);
+
+// Called everytime a weapon is chosen by user
+function play(e, playerSelection){
+    
+    if(!gameWon){
+        let computerSelection = computerPlay();
+
+        playerChoices.forEach(choice => choice.classList.remove('selected'));
+        compChoices.forEach(choice => choice.classList.remove('selected'));
+
+        document.querySelector(`.playerChoice#${playerSelection}`).classList.add('selected');
+        document.querySelector(`.compChoice#${computerSelection}`).classList.add('selected');
+        
+        console.log(playerSelection, computerSelection)
+        if(playerSelection==='rock' && computerSelection==='scissors')
+        result.textContent = "You Win! Rock beats Scissors!";
+        if(playerSelection==='rock' && computerSelection==='paper')
+        result.textContent = "You Lose! Paper beats Rock!";
+        if(playerSelection==='paper' && computerSelection==='scissors')
+        result.textContent = "You Lose! Scissors beats Paper!";
+        if(playerSelection==='paper' && computerSelection==='rock')
+        result.textContent = "You Win! Paper beats Rock!";
+        if(playerSelection==='scissors' && computerSelection==='paper')
+        result.textContent = "You Win! Scissors beats Paper!";
+        if(playerSelection==='scissors' && computerSelection==='rock')
+        result.textContent = "You Lose! Rock beats Scissors!";
+        if(playerSelection===computerSelection)
+        result.textContent = "Tie!";
+        
+        if(result.textContent.includes('You Win!')) playerScore += 1;
+        else if(result.textContent.includes('You Lose!')) computerScore += 1;
+        score.textContent = `${playerScore} - ${computerScore}`;
+        
+        if(playerScore === 5 || computerScore === 5){
+            gameWon = true;
+            gameEnd(result.textContent.match(/(You Win!|You Lose!)/g));
+        }
+        
     }
+    else gameEnd(result.textContent.match(/(You Win!|You Lose!)/g));
 
-    console.log('Game Over!');
-
-    if(playerScore > computerScore) console.log('Player Wins!');
-    else if(playerScore < computerScore) console.log('Computer Wins!');
-    else console.log('Tie!');
 }
 
-gamePlay();
+// Called to restart the game
+function restart(){
+    playerScore = 0;
+    computerScore = 0;
+    score.textContent = '0 - 0';
+    result.textContent = 'Play!';
+    finalResult.classList.remove('showResult');
+    gameWon = false;
+    playerChoices.forEach(choice => choice.classList.remove('selected'));
+    compChoices.forEach(choice => choice.classList.remove('selected'));
+}
+
+// Basic initialisation
+let playerScore = 0, computerScore = 0, gameWon = false;
+const result = document.querySelector('.result');
+const score = document.querySelector('.score');
+const selection = document.querySelector('.selection');
+const finalResult = document.querySelector('.finalResult');
+const playAgainBtn = document.querySelector('.playAgainBtn');
+const finalResultText = document.querySelector('.finalResultText');
+score.textContent = '0 - 0';
+result.textContent = 'Play!';
+const playerChoices = document.querySelectorAll('.playerChoice');
+const compChoices = document.querySelectorAll('.compChoice')
+playerChoices.forEach(button => button.addEventListener('click', (e) => play(e,button.textContent.toLowerCase())))
+
+
 
 
